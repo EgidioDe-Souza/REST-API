021ca8bfc1e7700bbc967f502b660ab3d764f49b
Egidio De-Souza <egidio.de-souza@hotmail.co.uk>
2021-11-03 12:12:23 +0000

    Created Model

--- .venv/application.py
+++ .venv/application.py
@@ -1,16 +1,23 @@
 from flask import Flask
 def create_app():
   app = Flask(__name__)
-
-  @app.route('/')
-  def index():
-    return 'Hello World'
   return app
 app = create_app()
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.author} - {self.description}"
 
 @app.route('/books')
 def get_books():
     return {"books": "books data "}
+
+
 
 
 
